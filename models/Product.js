23bdc0f73209935942e2f438fdec093fd45a36fc
Dave Sanders <davepaulsanders@gmail.com>
2022-05-13 10:05:15 -0400

    Refactoring

--- models/Product.js
+++ models/Product.js
@@ -34,13 +34,6 @@ Product.init(
         isNumeric: true,
       },
     },
-    category_id: {
-      type: DataTypes.INTEGER,
-      references: {
-        model: "category",
-        key: "id",
-      },
-    },
   },
   {
     sequelize,
--- models/Tag.js
+++ models/Tag.js
@@ -6,12 +6,6 @@ class Tag extends Model {}
 
 Tag.init(
   {
-    id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      primaryKey: true,
-      autoIncrement: true,
-    },
     tag_name: {
       type: DataTypes.STRING,
     },
